e55a30ade8c44fbed5c2e0fd1f421dddae03e828
deviltt <tt416926063@qq.com>
2019-03-08 20:21:05 +0800

    使用Map + String的方法与前端页面进行交互，同样前端传递过来的信息可以通过实体类参数自动封装

--- oa1/src/main/java/com/tt/oa/controller/DepartmentController.java
+++ oa1/src/main/java/com/tt/oa/controller/DepartmentController.java
@@ -6,10 +6,10 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/department")
@@ -20,70 +20,51 @@ public class DepartmentController {
     private HttpServletRequest request;
 
     @RequestMapping("/list")
-    public ModelAndView departmentList(){
-        ModelAndView modelAndView = new ModelAndView();
+    /**
+     * 功能：查询数据库中所有的Department，并将list集合传递给前端页面
+     */
+    public String departmentList(Map<String, Object> map){
         List<Department> departmentList = departmentService.listDepartment();
-        modelAndView.addObject("departments", departmentList);
-        modelAndView.setViewName("departmentlist");
-        return modelAndView;
+        map.put("departments", departmentList);
+        return "departmentlist";
     }
 
     @RequestMapping("/to_update")
-    public ModelAndView toUpdateDepartment(@Param("id")String id){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("department", departmentService.getDepartment(id));
-        modelAndView.setViewName("departmentupdate");
-        return modelAndView;
+    /**
+     * 去编辑部门的页面，因此需要携带待编辑部门的信息，并显示在页面对应输入框中
+     */
+    public String toUpdateDepartment(@Param("id")String id, Map<String, Object> map){
+        map.put("department", departmentService.getDepartment(id));
+        return "departmentupdate";
     }
 
     @RequestMapping("/remove")
     public String removeDepartment(@Param("id")String id){
-//        ModelAndView modelAndView = new ModelAndView();
         departmentService.deleteDepartment(id);
         return "redirect:list";
-
-//        List<Department> departments = departmentService.listDepartment();
-//        modelAndView.addObject("departments", departments);
-//        modelAndView.setViewName("departmentlist");
-//        return modelAndView;
     }
 
     @RequestMapping("/update")
-    public String updateDepartment(@Param("departmentName")String departmentName, @Param("departmentPosition")String departmentPosition){
+    /**
+     * 对部门进行修改，前端输入的信息会自动的绑定到参数对象中去
+     */
+    public String updateDepartment(Department department){
         String id = request.getParameter("id");
-//        ModelAndView modelAndView = new ModelAndView();
-        Department department = new Department(id, departmentName, departmentPosition);
         departmentService.updateDepartment(department);
         return "redirect:list";
-
-//        List<Department> departments = departmentService.listDepartment();
-//        modelAndView.addObject("departments", departments);
-//        modelAndView.setViewName("departmentlist");
-//        return modelAndView;
     }
 
     @RequestMapping("/to_add")
-    public ModelAndView toAddDepartment(){
+    public String toAddDepartment(Map<String, Object> map){
         Department department = new Department();
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("department", department);
-        modelAndView.setViewName("departmentadd");
-        return modelAndView;
+        map.put("department", department);
+        return "departmentadd";
     }
 
     @RequestMapping("addDepartment")
-    public String addDepartment(){
-//        ModelAndView modelAndView = new ModelAndView();
-        String id = request.getParameter("id");
-        String departmentName = request.getParameter("departmentName");
-        String departmentPosition = request.getParameter("departmentPosition");
-        Department department = new Department(id, departmentName, departmentPosition);
+    //在方法参数中申明department，则前台的数据会自动绑定到参数的Department对象中去
+    public String addDepartment(Department department){
         departmentService.addDepartment(department);
         return "redirect:list";
-
-//        List<Department> departments = departmentService.listDepartment();
-//        modelAndView.addObject("departments", departments);
-//        modelAndView.setViewName("departmentlist");
-//        return modelAndView;
     }
 }
